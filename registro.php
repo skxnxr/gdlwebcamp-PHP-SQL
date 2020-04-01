29acd243c85d3654f5ae5de5c6e3c643cde2baca
Skyner <sacwks@gmailcom>
2020-04-01 05:02:26 -0400

    Finalizado el video 478

--- registro.php
+++ registro.php
@@ -1,5 +1,6 @@
 <?php include_once 'includes/templates/header.php'; ?>
 
+
     <section class="section contenedor">
         <h2>Registro de usuarios</h2>
         <form id="registro" class="registro" action="pagar.php" method="POST">
@@ -99,7 +100,11 @@
                         $eventos_dias = array();
                         while ($eventos = $resultado->fetch_assoc()) {
                             $fecha = $eventos['fecha_evento'];
-                            setlocale(LC_ALL,"es_ES");
+                           
+                            //Spanish
+                            $spanish = array('es_utf8', 'es', 'es-ES', 'Spanish_Modern_Sort', 'es_utf8', 'es_ES@euro', 'esp_esp', 'esp_spain', 'spanish_esp', 'spanish_spain', 'es_ES.utf8', 'es-es');
+                            setlocale(LC_ALL, $spanish);
+                            //----------------------------------------------
                             $dia_semana = strftime("%A", strtotime($fecha));
                             $categoria = $eventos['cat_evento'];
                             $dia = array(
@@ -111,76 +116,32 @@
                             );
                             $eventos_dias[$dia_semana]['eventos'][$categoria][] = $dia;
                         } 
-                         echo "<pre>";
-                         var_dump($eventos_dias);
-                         echo "</pre>";
+                        //  echo "<pre>";
+                        //  var_dump($eventos_dias);
+                        //  echo "</pre>";
+                        
+                        //var_dump(setlocale(LC_TIME , 'es_ES.UTF-8'));
                     ?>
-                      <div id="viernes" class="contenido-dia clearfix">
-                          <h4>Viernes</h4>
-                              <div>
-                                  <p>Talleres:</p>
-                                  <label><input type="checkbox" name="registro[]" id="taller_01" value="taller_01"><time>10:00</time> Responsive Web Design</label>
-                                  <label><input type="checkbox" name="registro[]" id="taller_02" value="taller_02"><time>12:00</time> Flexbox</label>
-                                  <label><input type="checkbox" name="registro[]" id="taller_03" value="taller_03"><time>14:00</time> HTML5 y CSS3</label>
-                                  <label><input type="checkbox" name="registro[]" id="taller_04" value="taller_04"><time>17:00</time> Drupal</label>
-                                  <label><input type="checkbox" name="registro[]" id="taller_05" value="taller_05"><time>19:00</time> WordPress</label>
+                    <?php foreach ($eventos_dias as $dia => $eventos) { ?>
+                        
+                    
+                      <div id="<?php $dia = utf8_encode($dia); echo str_replace('á', 'a', $dia); ?>" class="contenido-dia clearfix">
+                          <h4><?php echo $dia; ?></h4>
+                          <?php foreach($eventos['eventos'] as $tipo => $eventos_dias): ?>
+                              <div> 
+                                  <p><?php echo $tipo; ?>:</p>
+                                    <?php foreach($eventos_dias as $evento){ ?>
+                                        <label>
+                                            <input type="checkbox" name="registro[]" id="<?php echo $evento['id']; ?>" value="<?php echo $evento['id']; ?>">
+                                            <time><?php echo $evento['hora']; ?></time><?php echo " " . $evento['nombre_evento']; ?>
+                                            <br>
+                                            <span class="autor"><?php echo $evento['nombre_invitado'] . " " . $evento['apellido_invitado']; ?></span>
+                                        </label>
+                                    <?php } //foreach ?> 
                               </div>
-                              <div>
-                                  <p>Conferencias:</p>
-                                  <label><input type="checkbox" name="registro[]" id="conf_01" value="conf_01"><time>10:00</time> Como ser Freelancer</label>
-                                  <label><input type="checkbox" name="registro[]" id="conf_02" value="conf_02"><time>17:00</time> Tecnologías del Futuro</label>
-                                  <label><input type="checkbox" name="registro[]" id="conf_03" value="conf_03"><time>19:00</time> Seguridad en la Web</label>
-                              </div>
-                              <div>
-                                  <p>Seminarios:</p>
-                                  <label><input type="checkbox" name="registro[]" id="sem_01" value="sem_01"><time>10:00</time> Diseño UI y UX para móviles</label>
-                              </div>
-                      </div> <!--#viernes-->
-                      <div id="sabado" class="contenido-dia clearfix">
-                          <h4>Sábado</h4>
-                          <div>
-                                <p>Talleres:</p>
-                                <label><input type="checkbox" name="registro[]" id="taller_06" value="taller_06"><time>10:00</time> AngularJS</label>
-                                <label><input type="checkbox" name="registro[]" id="taller_07" value="taller_07"><time>12:00</time> PHP y MySQL</label>
-                                <label><input type="checkbox" name="registro[]" id="taller_08" value="taller_08"><time>14:00</time> JavaScript Avanzado</label>
-                                <label><input type="checkbox" name="registro[]" id="taller_09" value="taller_09"><time>17:00</time> SEO en Google</label>
-                                <label><input type="checkbox" name="registro[]" id="taller_10" value="taller_10"><time>19:00</time> De Photoshop a HTML5 y CSS3</label>
-                                <label><input type="checkbox" name="registro[]" id="taller_11" value="taller_11"><time>21:00</time> PHP Medio y Avanzado</label>
-                          </div>
-                          <div>
-                                <p>Conferencias:</p>
-                                <label><input type="checkbox" name="registro[]" id="conf_04" value="conf_04"><time>10:00</time> Como crear una tienda online que venda millones en pocos días</label>
-                                <label><input type="checkbox" name="registro[]" id="conf_05" value="conf_05"><time>17:00</time> Los mejores lugares para encontrar trabajo</label>
-                                <label><input type="checkbox" name="registro[]" id="conf_06" value="conf_06"><time>19:00</time> Pasos para crear un negocio rentable</label>
-                          </div>
-                          <div>
-                                <p>Seminarios:</p>
-                                <label><input type="checkbox" name="registro[]" id="sem_02" value="sem_02"><time>10:00</time> Aprende a Programar en una mañana</label>
-                                <label><input type="checkbox" name="registro[]" id="sem_03" value="sem_03"><time>17:00</time> Diseño UI y UX para móviles</label>
-                          </div>
-                      </div> <!--#sabado-->
-                      <div id="domingo" class="contenido-dia clearfix">
-                          <h4>Domingo</h4>
-                          <div>
-                                <p>Talleres:</p>
-                                <label><input type="checkbox" name="registro[]" id="taller_12" value="taller_12"><time>10:00</time> Laravel</label>
-                                <label><input type="checkbox" name="registro[]" id="taller_13" value="taller_13"><time>12:00</time> Crea tu propia API</label>
-                                <label><input type="checkbox" name="registro[]" id="taller_14" value="taller_14"><time>14:00</time> JavaScript y jQuery</label>
-                                <label><input type="checkbox" name="registro[]" id="taller_15" value="taller_15"><time>17:00</time> Creando Plantillas para WordPress</label>
-                                <label><input type="checkbox" name="registro[]" id="taller_16" value="taller_16"><time>19:00</time> Tiendas Virtuales en Magento</label>
-                          </div>
-                          <div>
-                                <p>Conferencias:</p>
-                                <label><input type="checkbox" name="registro[]" id="conf_07" value="conf_07"><time>10:00</time> Como hacer Marketing en línea</label>
-                                <label><input type="checkbox" name="registro[]" id="conf_08" value="conf_08"><time>17:00</time> ¿Con que lenguaje debo empezar?</label>
-                                <label><input type="checkbox" name="registro[]" id="conf_09" value="conf_09"><time>19:00</time> Frameworks y librerias Open Source</label>
-                          </div>
-                          <div>
-                                <p>Seminarios:</p>
-                                <label><input type="checkbox" name="registro[]" id="sem_04" value="sem_04"><time>14:00</time> Creando una App en Android en una tarde</label>
-                                <label><input type="checkbox" name="registro[]" id="sem_05" value="sem_05"><time>17:00</time> Creando una App en iOS en una tarde</label>
-                          </div>
-                      </div> <!--#domingo-->
+                          <?php endforeach; ?>    
+                      </div> <!--#día-->
+                    <?php } ?>    
                   </div><!--.caja-->
             </div> <!--#eventos-->
 
